e8f314a5995a3b64ed45cf348d4a3252951e9136
Chavez <matthew@el-chavez.me>
2016-03-12 21:25:26 -0800

    Remove /organization endpoint
    * New endpoint is /recent-builds

--- spec/circleci_spec.rb
+++ spec/circleci_spec.rb
@@ -18,20 +18,4 @@ describe CircleCi do
       CircleCi.config.token.should eql 'new-key'
     end
   end
-
-  describe 'organization', vcr: { cassette_name: 'organization', record: :none } do
-    subject { CircleCi.organization(ENV['ORGANIZATION'] || 'orga-name') }
-    let(:body) { subject.body }
-
-    it { should be_an_instance_of(CircleCi::Response) }
-    it { should be_success }
-
-    it 'returns a list of recent builds' do
-      expect(body).to be_an_instance_of(Array)
-      expect(body.size).to eq(2)
-      expect(body.first).to have_key('username')
-      expect(body.first).to have_key('reponame')
-      expect(body.first).to have_key('vcs_url')
-    end
-  end
 end
