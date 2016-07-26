500ddb46aee92f3677d3ebc42b0f54ba6e3f99dc
Chavez <matthew@el-chavez.me>
2016-07-25 17:49:12 -0700

    Correct comment on deprecated parsed_body method

--- lib/circleci/response.rb
+++ lib/circleci/response.rb
@@ -5,11 +5,11 @@ module CircleCi
   # Response class is used to get access to raw HTTP request info
   class Response
     attr_reader :success, :body, :errors, :code, :path
+
     ##
     # Initializing response object to be returned from API calls, used internally.
     #
     # @private
-
     def initialize(http, resp_code, resp_path) # @private
       @success = http.success
       @body = http.response
@@ -22,17 +22,14 @@ module CircleCi
     #
     # Convenience method to determine if request was successfull or not
     # @return [Boolean]
-
     def success?
       @success == true
     end
 
     ##
     #
-    # *Deprecated* - To remove hashie dependency
-    # Parses JSON body of request
     # @return [@body]
-
+    # @deprecated No longer requires hashie to wrap response body
     def parsed_body
       @body
     end
