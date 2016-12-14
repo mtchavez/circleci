718aa5f35a613bc8a240ea6d255acba7d3d62b8c
Austin Emmons <austin.emmons@modolabs.com>
2016-12-14 12:15:34 -0500

    Update project to use instance methods

--- lib/circleci/project.rb
+++ lib/circleci/project.rb
@@ -4,8 +4,9 @@ module CircleCi
   #
   # Class for interacting with Projects
   class Project
+    attr_reader :vcs_type, :username, :name
+
     ##
-    #
     # Return all projects for your API key
     #
     # @return [CircleCi::Response] - Response object
@@ -14,223 +15,189 @@ module CircleCi
     end
 
     ##
+    # Initialize project
     #
+    # @param vcs_type [String] - Either :github or :bitbucket, the version control system used for this project.
+    # @param username [String] - Name of version control user/organization
+    # @param project_name [String] - Name of the project in the version control system
+    def initialize(vcs_type, username, project_name)
+      @vcs_type = vcs_type.to_s
+      @username = username
+      @name     = project_name
+    end
+
+    ##
     # Build the latest master push for this project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @return         [CircleCi::Response] - Response object
-    def self.build(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}").post
+    def build
+      CircleCi.request(base_path).post
     end
 
     ##
-    #
     # Build the latest push for this branch of a specific project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param branch   [String] - Name of branch
     # @param body     [Hash] - Optional post body with build parameters
+    #
     # @return         [CircleCi::Response] - Response object
-    def self.build_branch(username, project, branch, params = {}, body = {})
-      CircleCi.request("/#{project_path}/#{username}/#{project}/tree/#{branch}", params).post(body)
+    def build_branch(branch, params = {}, body = {})
+      CircleCi.request("#{base_path}/tree/#{branch}", params).post(body) 
     end
 
     ##
-    #
     # Add a ssh key to a build
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param build    [String] - Build number
     # @param key      [String] - The ssh private key
     # @param hostname [String] - The hostname identified by the key
+    #
     # @return         [CircleCi::Response] - Response object
-    def self.build_ssh_key(username, project, build, key, hostname)
+    def build_ssh_key(build, key, hostname)
       body = { hostname: hostname, private_key: key }
-      CircleCi.request("/#{project_path}/#{username}/#{project}/#{build}/ssh-users").post(body)
+      CircleCi.request("#{base_path}/#{build}/ssh-users").post(body)
     end
 
     ##
-    #
     # Clear the build cache for a specific project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @return         [CircleCi::Response] - Response object
-    def self.clear_cache(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/build-cache").delete
+    def clear_cache()
+      CircleCi.request("#{base_path}/build-cache").delete
     end
 
     ##
-    #
     # Delete a checkout key for a project
     #
-    # @param username     [String] - User or org name who owns project
-    # @param project      [String] - Name of project
     # @param fingerprint  [String] - Fingerprint of a checkout key
+    #
     # @return             [CircleCi::Response] - Response object
-    def self.delete_checkout_key(username, project, fingerprint)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/checkout-key/#{fingerprint}").delete
+    def delete_checkout_key(fingerprint)
+      CircleCi.request("#{base_path}/checkout-key/#{fingerprint}").delete
     end
 
     ##
-    #
     # Enable a project in CircleCI. Causes a CircleCI SSH key to be added to
     # the GitHub. Requires admin privilege to the repository.
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @return         [CircleCi::Response] - Response object
-    def self.enable(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/enable").post
+    def enable
+      CircleCi.request("#{base_path}/enable").post
     end
 
     ##
-    #
     # Get the project envvars
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @return         [CircleCi::Response] - Response object
-    def self.envvar(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/envvar").get
+    def envvar
+      CircleCi.request("#{base_path}/envvar").get
     end
 
-    ##
-    #
-    # @deprecated Please use [CircleCi::Project#envvar]
-    def self.envvars(username, project)
-      # NOTE: Make logger configurable on config object?
-      logger = Logger.new(STDOUT)
-      logger.warn('[Deprecated] Project#envvars is deprecated please use Project#envvar')
-      envvar username, project
-    end
 
     ##
-    #
     # Sets an envvar for a project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param envvar   [Hash] - {name: 'foo', value: 'bar'}
+    #
     # @return         [CircleCi::Response] - Response object
-    def self.set_envvar(username, project, envvar)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/envvar").post(envvar)
+    def set_envvar(envvar)
+      CircleCi.request("#{base_path}/envvar").post(envvar)
     end
 
     ##
-    #
     # Follow the project
     #
     # @param username [String] - User or org name who owns project
     # @param project  [String] - Name of project
+    #
     # @return         [CircleCi::Response] - Response object
-    def self.follow(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/follow").post
+    def follow
+      CircleCi.request("#{base_path}/follow").post
     end
 
     ##
-    #
     # Get a checkout key for a project
     #
     # @param username     [String] - User or org name who owns project
     # @param project      [String] - Name of project
     # @param fingerprint  [String] - Fingerprint of a checkout key
+    #
     # @return             [CircleCi::Response] - Response object
-    def self.get_checkout_key(username, project, fingerprint)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/checkout-key/#{fingerprint}").get
+    def get_checkout_key(fingerprint)
+      CircleCi.request("#{base_path}/checkout-key/#{fingerprint}").get
     end
 
     ##
-    #
     # Get a list of checkout keys for project
     #
     # @param username [String] - User or org name who owns project
     # @param project  [String] - Name of project
+    #
     # @return         [CircleCi::Response] - Response object
-    def self.list_checkout_keys(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/checkout-key").get
+    def list_checkout_keys
+      CircleCi.request("#{base_path}/checkout-key").get
     end
 
     ##
-    #
     # Create a checkout key for a project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param type     [String] - The type of key to create. Can be 'deploy-key' or 'github-user-key'.
     # @return         [CircleCi::Response] - Response object
 
-    def self.new_checkout_key(username, project, type)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/checkout-key").post(type: type)
+    def new_checkout_key(username, project, type)
+      CircleCi.request("#{base_path}/checkout-key").post(type: type)
     end
 
     ##
-    #
     # Get all recent builds for a specific project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param params   [Hash] - Parameters for builds (limit, offset, filter)
     # @return         [CircleCi::Response] - Response object
 
-    def self.recent_builds(username, project, params = {})
-      CircleCi.request("/#{project_path}/#{username}/#{project}", params).get
+    def recent_builds(params = {})
+      CircleCi.request("#{base_path}", params).get
     end
 
     ##
-    #
     # Get all recent builds for a specific branch of a project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param branch   [String] - Name of branch
+    #
     # @return         [CircleCi::Response] - Response object
-    def self.recent_builds_branch(username, project, branch)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/tree/#{branch}").get
+    def recent_builds_branch(branch)
+      CircleCi.request("#{base_path}/tree/#{branch}").get
     end
 
     ##
-    #
     # Get the project configuration
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @return         [CircleCi::Response] - Response object
-    def self.settings(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/settings").get
+    def settings
+      CircleCi.request("#{base_path}/settings").get
     end
 
     ##
-    #
     # Add a ssh key to a project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @param key      [String] - The ssh private key
     # @param hostname [String] - The hostname identified by the key
     # @return         [CircleCi::Response] - Response object
-    def self.ssh_key(username, project, key, hostname)
+    def add_ssh_key(key, hostname)
       body = { hostname: hostname, private_key: key }
-      CircleCi.request("/#{project_path}/#{username}/#{project}/ssh-key").post(body)
+      CircleCi.request("#{base_path}/ssh-key").post(body)
     end
 
     ##
-    #
     # Unfollow the project
     #
-    # @param username [String] - User or org name who owns project
-    # @param project  [String] - Name of project
     # @return         [CircleCi::Response] - Response object
-    def self.unfollow(username, project)
-      CircleCi.request("/#{project_path}/#{username}/#{project}/unfollow").post
+    def unfollow
+      CircleCi.request("#{base_path}/unfollow").post
     end
+
   private 
-    def self.project_path
-      # could also leverage the /:vcs-type/:username/:project path pattern for this section
-      "project/#{CircleCi.config.vcs_type}"
+    def base_path
+      "/project/#{vcs_type}/#{username}/#{name}"
     end
   end
 end
